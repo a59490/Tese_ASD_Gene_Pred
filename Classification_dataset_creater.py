a441c7582e342c2e8a0695390c2a783be0229ef5
fc59490 <fc59490@alunos.fc.ul.pt>
2023-12-20 13:15:39 +0000

    updated the Classification_dataset_creater.py

--- Classification_dataset_creater.py
+++ Classification_dataset_creater.py
@@ -6,26 +6,22 @@ sfari= pd.read_csv("sfari_ed.csv") #open sfari file
 ensembl_id_column = sfari["ensembl-id"].to_list() #get ensembl id column
 
 xls = pd.read_excel("Negative_Positive_genes.xlsx", sheet_name=1)
-NCBI_id_column = xls["gene id"].to_list() #get NCBI id column
-NCBI_id=[str(id) for x in NCBI_id_column]
+NCBI_id_column = xls["gene id"].astype(str).to_list() #get NCBI id column
 
 final_list=[]
 
 with open("emb_file.csv", "r") as f:
     csv_reader = csv.reader(f)
-    next(csv_reader) 
+    next(csv_reader)
 
     for row in csv_reader:
         if len(row) == 6:
             if row[1] in ensembl_id_column:
                 final_list.append(row)
-            elif str(row[3]) in NCBI_id:
+            elif str(row[3]) in NCBI_id_column:
                 final_list.append(row)
-
-#write final list to csv file
 
 
 with open("final_dataset.csv", "w", newline="") as output_file:
     csv_writer = csv.writer(output_file)
     csv_writer.writerows(final_list)
-                
