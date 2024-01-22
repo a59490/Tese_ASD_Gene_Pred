287710bcd842436625c653a4f8796088886bdd51
fc59490 <fc59490@alunos.fc.ul.pt>
2024-01-22 17:49:52 +0000

    changed the SFARI dataset to a newer version

--- 3_pos_neg_genes/Classification_dataset_creater.py
+++ 3_pos_neg_genes/Classification_dataset_creater.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd
 import numpy as np
 
-gene_list = pd.read_csv("gene_list.csv", index_col=0)
+gene_list = pd.read_csv("gene_list_01_16_2024.csv", index_col=0)
 
 final_list=[]
 
