c5e76f36694e7a7e5177a2cecaf3d4485b690f31
fc59490 <fc59490@alunos.fc.ul.pt>
2024-02-10 13:18:09 +0000

    create the prot_emb file

--- 2_Emb_seq_maker/prott5/prot_emb.py
+++ 2_Emb_seq_maker/prott5/prot_emb.py
@@ -5,39 +5,34 @@ import re
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# Load the tokenizer
 tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_half_uniref50-enc', do_lower_case=False)
 
-# Load the model
 model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc").to(device)
 
-# prepare your protein sequences as a list
-protein_sequences = ["PRTEINO", "SEQWENCE"]
 
-# replace all rare/ambiguous amino acids by X and introduce white-space between all amino acids
-protein_sequences = [" ".join(list(re.sub(r"[UZOB]", "X", sequence))) for sequence in protein_sequences]
 
-# List to hold all the protein embeddings
-all_embeddings = []
+with open("peptide_seq.csv", "r") as f:
+    csv_reader = csv.reader(f)
+    next(csv_reader) 
+    for row in csv_reader:
+            if row[3] != "None":
+                try:
+                    processed_sequence = " ".join(list(re.sub(r"[UZOB]", "X", row[3])))
+                    ids = tokenizer(processed_sequence, add_special_tokens=True, padding="longest", return_tensors="pt").to(device)
+                    input_ids = ids['input_ids']
+                    attention_mask = ids['attention_mask']
 
-# Iterate over each protein sequence
-for sequence in protein_sequences:
-    # tokenize sequences and pad up to the longest sequence in the batch
-    ids = tokenizer(sequence, add_special_tokens=True, padding="longest", return_tensors="pt").to(device)
-    input_ids = ids['input_ids']
-    attention_mask = ids['attention_mask']
+                    with torch.no_grad():
+                        embedding_repr = model(input_ids=input_ids, attention_mask=attention_mask)
+                    protein_embedding = embedding_repr.last_hidden_state.mean(dim=1)[0]  # shape (1024,)
 
-    # generate embeddings
-    with torch.no_grad():
-        embedding_repr = model(input_ids=input_ids, attention_mask=attention_mask)
+                    protein_embedding = protein_embedding.tolist()
 
-    # extract residue embeddings and remove padded & special tokens
-    per_protein_embedding = embedding_repr.last_hidden_state.mean(dim=1)[0]  # shape (1024,)
-    
-    # Append the embedding to the list
-    all_embeddings.append(per_protein_embedding.tolist())
+                    row=row[:3]
+                    row.append(protein_embedding)
+                    with open("prot_emb.csv", "a", newline='') as file:
+                        csv_writer = csv.writer(file)
+                        csv_writer.writerow(row)
+                except:
+                    continue
 
-# Write all the embeddings to a single CSV file
-with open("all_protein_embeddings.csv", "w", newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(all_embeddings)
