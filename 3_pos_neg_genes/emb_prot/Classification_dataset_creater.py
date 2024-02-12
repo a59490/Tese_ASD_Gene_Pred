5ea8f67c8367307504ce02c5756c065f00fbfca6
fc59490 <fc59490@alunos.fc.ul.pt>
2024-02-12 16:08:18 +0000

    created prot ml eval

--- 3_pos_neg_genes/emb_prot/Classification_dataset_creater.py
+++ 3_pos_neg_genes/emb_prot/Classification_dataset_creater.py
@@ -13,10 +13,14 @@ with gzip.open("prot_emb.csv.gz", "rt") as f:
     for row in csv_reader:
         if len(row) == 4:
             if row[1] in gene_list.index:
+
+                row[3]=row[3].strip("[]")
+                row[3] = [float(value) for value in row[3].split(",")]
+
                 row = row + [gene_list.loc[row[1], "y"]]
                 final_list.append(row)
 
 # Write final list to csv file
-with open("final_dataset_max.csv", "w", newline="") as output_file:
+with open("dataset_prot.csv", "w", newline="") as output_file:
     csv_writer = csv.writer(output_file)
     csv_writer.writerows(final_list)
---
+++ 4_ML/3_prot/submit3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=100:00:00
+
+# set the number of tasks (processes) per node.
+#SBATCH --ntasks-per-node=16
+
+# set partition
+#SBATCH --partition=BIOISI
+
+# set name of job
+#SBATCH --job-name=ASD_PRED
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=NONE
+
+# send mail to this address
+#SBATCH --mail-user=joaopinacio96@gmail.com
+
+# run the application
+echo "Now on: $PWD"
+$HOME/miniconda3/bin/conda run -n dna python /work/joaoinacio/work/Tese_ASD_Gene_Pred/4_ML/3_prot/ml_results.py
+echo "Finished"
