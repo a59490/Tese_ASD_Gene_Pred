c33e9339386c4dd33589ef7a8943be6511fc518b
fc59490 <fc59490@alunos.fc.ul.pt>
2024-02-22 11:30:52 +0000

    updated submit

---
+++ 4_ML/1_ed_negative_list/submit_arg.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=100:00:00
+
+# set the number of tasks (processes) per node.
+#SBATCH --ntasks-per-node=16
+
+# set partition
+#SBATCH --partition=BIOISI
+
+# set name of job
+#SBATCH --job-name=ASD_PRED
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=NONE
+
+# send mail to this address
+#SBATCH --mail-user=joaopinacio96@gmail.com
+
+export OMP_NUM_THREADS=16
+
+# run the application
+echo "Now on: $PWD"
+$HOME/miniconda3/bin/conda run -n sklearn-env python /work/joaoinacio/work/Tese_ASD_Gene_Pred/4_ML/1_ed_negative_list/argparse_test.py lr
+echo "Finished"
