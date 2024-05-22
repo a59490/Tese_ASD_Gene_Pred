e204fa7b25be8eb62a1b4025b12fd65c1c4d7009
João Inácio <fc59490@alunos.fc.ul.pt>
2024-05-22 11:17:43 +0100

    added the pipeline to try diferent graph embs

---
+++ 14_graph_embeddings/Create_embs.py
@@ -0,0 +1,64 @@
+from grape import Graph
+import pandas as pd
+
+from grape import GraphVisualizer
+from grape.embedders import FirstOrderLINEEnsmallen
+from grape.embedders import SecondOrderLINEEnsmallen
+
+from grape.embedders import Node2VecSkipGramEnsmallen
+from grape.embedders import Node2VecCBOWEnsmallen
+
+from grape.embedders import DeepWalkSkipGramEnsmallen
+from grape.embedders import DeepWalkCBOWEnsmallen
+
+from  grape.embedders import WalkletsCBOWEnsmallen
+from grape.embedders import WalkletsSkipGramEnsmallen
+
+from grape.embedders import HOPEEnsmallen
+from grape.embedders import GLEEEnsmallen
+
+
+csv_file_path = "string_protein_links.txt"
+
+separator = " "  
+
+# Create the graph from the CSV file
+string_graph = Graph.from_csv(
+    edge_path=csv_file_path,
+    edge_list_separator=separator,
+    sources_column_number=0,  # protein 1 is in the first column
+    destinations_column_number=1,  # Aprotein 2 is in the second column
+    weights_column_number=2,  # Assuming weight is in the third column ------------- /1000 check later!!!!!!!!
+    directed=False,  #  graph is undirected, adjust if necessary
+    verbose=True  # Optionally, set verbose to True for more information during loading
+)
+
+string_graph.remove_disconnected_nodes()
+
+embedding_list = [("first_order_line", FirstOrderLINEEnsmallen(embedding_size=200)), ("second_order_line", SecondOrderLINEEnsmallen(embedding_size=200)),
+                   ("node2vec_skipgram", Node2VecSkipGramEnsmallen(embedding_size=200)), ("node2vec_cbow", Node2VecCBOWEnsmallen(embedding_size=200)),
+                     ("glee", GLEEEnsmallen(embedding_size=200)), ("deepwalk_skipgram", DeepWalkSkipGramEnsmallen(embedding_size=200)),
+                       ("deepwalk_cbow", DeepWalkCBOWEnsmallen(embedding_size=200)), ("hope", HOPEEnsmallen(embedding_size=200)),("walklets_cbow", WalkletsCBOWEnsmallen(embedding_size=200)),
+                         ("walklets_skipgram", WalkletsSkipGramEnsmallen(embedding_size=200))]
+
+def make_embedding(embedding_list, graph):
+    for emb_name,algorithm in embedding_list:
+        embedding = algorithm.fit_transform(graph)
+        if len(embedding.get_all_node_embedding())==2:
+            results_df=embedding.get_all_node_embedding()[0]
+            results_df.to_csv(f"Embeddings/{emb_name}_embedding.csv")
+
+        else:
+            results_df=embedding.get_all_node_embedding()[0] # this is the central tokens, more useful for skipgram
+            results_df.to_csv(f"Embeddings/{emb_name}_embedding.csv")
+
+def clean_embedding(embedding_list):
+    for emb_name,algorithm in embedding_list:
+        results_df=pd.read_csv(f"Embeddings/{emb_name}_embedding.csv")
+        converter=pd.read_csv("prot_converter.csv")
+        new_emb = pd.merge(results_df, converter, left_on="Unnamed: 0", right_on='ensb_gene_id',how='inner')
+        new_emb.to_csv(f"Clean_embeddings/{emb_name}_embedding.csv")
+            
+
+make_embedding(embedding_list,string_graph)
+clean_embedding(embedding_list)
---
+++ 14_graph_embeddings/submit_embs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=200:00:00
+
+# set the number of tasks (processes) per node.
+#SBATCH --ntasks-per-node=16
+
+# set partition
+#SBATCH --partition=BIOISI
+
+# set name of job
+#SBATCH --job-name=ASD_EMB
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=NONE
+
+# send mail to this address
+#SBATCH --mail-user=joaopinacio96@gmail.com
+
+export OMP_NUM_THREADS=16
+
+# run the application
+echo "Now on: $PWD"
+$HOME/miniconda3/bin/conda run -n sklearn-env python /work/joaoinacio/work/Tese_ASD_Gene_Pred/14_graph_embeddings/Create_embs.py
+echo "Finished"
